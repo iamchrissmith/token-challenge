809ebf8623b2d39ac7bb7c6eee7870a895860109
Chris Smith <me@chrisryansmith.com>
2017-11-14 09:01:31 -0700

    add open_zeppelin ownable contract

--- contracts/Whitelist.sol
+++ contracts/Whitelist.sol
@@ -3,19 +3,25 @@ pragma solidity ^0.4.8;
 import './Ownable.sol';
 
 contract Whitelist is Ownable {
+
+  function Whitelist() {}
+
   function addAddress(address _okay)
     public
     onlyOwner
-    returns(bool success);
+    returns(bool success)
+  {}
 
   function removeAddress(address _delete)
     public
     onlyOwner
-    returns(bool success);
+    returns(bool success)
+  {}
 
   function isWhitelisted(address _check)
     public
     constant
-    returns(bool isIndeed);
+    returns(bool isIndeed)
+  {}
   
 }
